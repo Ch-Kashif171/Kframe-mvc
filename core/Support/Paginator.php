a18b737a1537f5290561b45973e8eae42111f19b
Kashif Sohail <kashif@mawqiftech.com>
2025-07-30 18:48:30 +0500

    fix pagination ui

--- core/Support/Paginator.php
+++ core/Support/Paginator.php
@@ -8,102 +8,103 @@ class Paginator
      * @param $links
      * @return string
      */
-    public static  function pagination($links): string
+    public static function pagination($links): string
     {
-        $links = (object)$links;
+        $links = (object) $links;
         $html = '';
-        // Info line with safe defaults
+
         $from = $links->from ?? 0;
         $to = $links->to ?? 0;
         $total = $links->total ?? 0;
+
         if ($total > 0) {
-            $html .= '<div class="pagination-info">Showing ' . $from . ' to ' . $to . ' of ' . $total . ' entries</div>';
+            $html .= '<div class="d-flex pagination-info mb-2 justify-content-end mt-3">Showing ' . $from . ' to ' . $to . ' of ' . $total . ' entries</div>';
         }
+
         if (isset($links->last_page) && $links->last_page > 1) {
-            $html .= '<ul class="pagination">';
-            // Previous link (always shown, disabled if on first page)
+            $html .= '<div class="d-flex justify-content-end mt-3"><nav><ul class="pagination">';
+
+            // Previous
             if ($links->current_page <= 1) {
-                $html .= '<li class="disabled"><span>&laquo; Previous</span></li>';
+                $html .= '<li class="page-item disabled"><span class="page-link">&laquo; Previous</span></li>';
             } else {
-                $html .= '<li><a href="' . $links->prev_page_url . '">&laquo; Previous</a></li>';
+                $html .= '<li class="page-item"><a class="page-link" href="' . $links->prev_page_url . '">&laquo; Previous</a></li>';
             }
-            $window = 2; // Number of pages to show before/after current
+
+            $window = 2;
             $start = max(1, $links->current_page - $window);
             $end = min($links->last_page, $links->current_page + $window);
 
-            // Always show first page
+            // First page
             if ($start > 1) {
-                $html .= '<li><a href="' . $links->path . '?page=1">1</a></li>';
+                $html .= '<li class="page-item"><a class="page-link" href="' . $links->path . '?page=1">1</a></li>';
                 if ($start > 2) {
-                    $html .= '<li class="disabled"><span>...</span></li>';
+                    $html .= '<li class="page-item disabled"><span class="page-link">...</span></li>';
                 }
             }
 
             for ($i = $start; $i <= $end; $i++) {
-                $active = $i == $links->current_page ? ' class="active"' : '';
-                $html .= '<li' . $active . '><a href="' . $links->path . '?page=' . $i . '">' . $i . '</a></li>';
+                $active = $i == $links->current_page ? ' active' : '';
+                $html .= '<li class="page-item' . $active . '"><a class="page-link" href="' . $links->path . '?page=' . $i . '">' . $i . '</a></li>';
             }
 
-            // Always show last page
+            // Last page
             if ($end < $links->last_page) {
                 if ($end < $links->last_page - 1) {
-                    $html .= '<li class="disabled"><span>...</span></li>';
+                    $html .= '<li class="page-item disabled"><span class="page-link">...</span></li>';
                 }
-                $html .= '<li><a href="' . $links->path . '?page=' . $links->last_page . '">' . $links->last_page . '</a></li>';
+                $html .= '<li class="page-item"><a class="page-link" href="' . $links->path . '?page=' . $links->last_page . '">' . $links->last_page . '</a></li>';
             }
 
-            // Next link (always shown, disabled if on last page)
+            // Next
             if ($links->current_page >= $links->last_page) {
-                $html .= '<li class="disabled"><span>Next &raquo;</span></li>';
+                $html .= '<li class="page-item disabled"><span class="page-link">Next &raquo;</span></li>';
             } else {
-                $html .= '<li><a href="' . $links->next_page_url . '">Next &raquo;</a></li>';
+                $html .= '<li class="page-item"><a class="page-link" href="' . $links->next_page_url . '">Next &raquo;</a></li>';
             }
-            $html .= '</ul>';
+
+            $html .= '</ul></nav></div>';
         }
+
         return $html;
     }
+
 
     /**
      * @param $links
      * @return string
      */
-    public static  function simplePagination($links)
+    public static function simplePagination($links)
     {
-
         $html = '';
         $show = self::showPages($links);
 
         if ($show != 0) {
-            if (isset($_GET['page'])) {
-                $page = $_GET['page'];
-            } else {
-                $page = 1;
-            }
+            $page = $_GET['page'] ?? 1;
 
+            // Previous button
             if ($page == 1) {
-                $prev = '<span class="page-link">&laquo; Previous</span>';
-                $disabled = 'disabled';
+                $html .= '<ul class="pagination d-flex justify-content-end mt-3">';
+                $html .= '<li class="page-item disabled" aria-disabled="true">
+                        <span class="page-link">&laquo; Previous</span>
+                      </li>';
             } else {
-                $prev = ' <a href="' . $links->prev_page_url . '">Previous</a>';
-                $disabled = '';
+                $html .= '<ul class="pagination d-flex justify-content-end mt-3">';
+                $html .= '<li class="page-item">
+                        <a class="page-link" href="' . $links->prev_page_url . '" rel="prev">&laquo; Previous</a>
+                      </li>';
             }
 
-            $html = '<ul class="pagination" role="navigation">
-        <li class="page-item ' . $disabled . '" aria-disabled="true">
-        ' . $prev . '
-        </li>';
-
+            // Next button
             if ($page < $show) {
-                $nexLink = $links->next_page_url;
-                $disabled = '';
+                $html .= '<li class="page-item">
+                        <a class="page-link" href="' . $links->next_page_url . '" rel="next">Next &raquo;</a>
+                      </li>';
             } else {
-                $disabled = 'disabled';
-                $nexLink = 'javascript:void(0);';
-
+                $html .= '<li class="page-item disabled" aria-disabled="true">
+                        <span class="page-link">Next &raquo;</span>
+                      </li>';
             }
-
-            $html .= '<li class="page-item ' . $disabled . '">
-            <a class="page-link" href="' . $nexLink . '" rel="next">Next &raquo;</a></li>';
 
             $html .= '</ul>';
         }
